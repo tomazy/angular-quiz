e14bcf4103582f6b2d24eb7c84fb7bb410a193f6
Tomek Maszkowski <pair+tomek@neo.com>
2013-08-19 00:19:42 +0800

    Upgraded to the latest unstable angular

--- karma-e2e.conf.js
+++ karma-e2e.conf.js
@@ -1,58 +1,60 @@
 // Karma E2E configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(karma){
+  // base path, that will be used to resolve files and exclude
+  karma.basePath = '';
 
-// list of files / patterns to load in the browser
-files = [
-  ANGULAR_SCENARIO,
-  ANGULAR_SCENARIO_ADAPTER,
-  '.tmp/test/e2e/**/*.js'
-];
+  karma.frameworks = ['ng-scenario']
 
-// list of files to exclude
-exclude = [];
+  // list of files / patterns to load in the browser
+  karma.files = [
+    '.tmp/test/e2e/**/*.js'
+  ];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+  // list of files to exclude
+  karma.exclude = [];
 
-// web server port
-port = 8080;
+  // test results reporter to use
+  // possible values: dots || progress || growl
+  karma.reporters = ['progress'];
 
-// cli runner port
-runnerPort = 9100;
+  // web server port
+  karma.port = 8080;
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+  // cli runner port
+  karma.runnerPort = 9100;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+  // enable / disable colors in the output (reporters and logs)
+  karma.colors = true;
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+  // level of logging
+  // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+  karma.logLevel = karma.LOG_INFO;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+  // enable / disable watching file and executing tests whenever any file changes
+  karma.autoWatch = false;
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+  // Start these browsers, currently available:
+  // - Chrome
+  // - ChromeCanary
+  // - Firefox
+  // - Opera
+  // - Safari (only Mac)
+  // - PhantomJS
+  // - IE (only Windows)
+  karma.browsers = ['Chrome'];
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+  // If browser does not capture in given timeout [ms], kill it
+  karma.captureTimeout = 5000;
 
-// Uncomment the following lines if you are using grunt's server to run the tests
-proxies = {
-  '/': 'http://localhost:8888/'
-};
-// URL root prevent conflicts with the site root
-urlRoot = '_karma_';
+  // Continuous Integration mode
+  // if true, it capture browsers, run tests and exit
+  karma.singleRun = false;
+
+  // Uncomment the following lines if you are using grunt's server to run the tests
+  karma.proxies = {
+    '/': 'http://localhost:8888/'
+  };
+  // URL root prevent conflicts with the site root
+  karma.urlRoot = '_karma_';
+}
--- karma.conf.js
+++ karma.conf.js
@@ -1,57 +1,59 @@
 // Karma configuration
 
-// base path, that will be used to resolve files and exclude
-basePath = '';
+module.exports = function(karma){
+  // base path, that will be used to resolve files and exclude
+  karma.basePath = '';
 
-// list of files / patterns to load in the browser
-files = [
-  JASMINE,
-  JASMINE_ADAPTER,
-  'app/bower_components/angular/angular.js',
-  'app/bower_components/angular-mocks/angular-mocks.js',
-  '.tmp/scripts/**/*.js',
-  '.tmp/test/spec/specHelper.js',
-  '.tmp/test/spec/**/*Spec.js'
-];
+  karma.frameworks = ['jasmine'];
 
-// list of files to exclude
-exclude = [
-  '.tmp/scripts/modules/firebase_simple_login.js'
-];
+  // list of files / patterns to load in the browser
+  karma.files = [
+    'app/bower_components/angular/angular.js',
+    'app/bower_components/angular-mocks/angular-mocks.js',
+    '.tmp/scripts/**/*.js',
+    '.tmp/test/spec/specHelper.js',
+    '.tmp/test/spec/**/*Spec.js'
+  ];
 
-// test results reporter to use
-// possible values: dots || progress || growl
-reporters = ['progress'];
+  // list of files to exclude
+  karma.exclude = [
+    '.tmp/scripts/modules/firebase_simple_login.js'
+  ];
 
-// web server port
-port = 8080;
+  // test results reporter to use
+  // possible values: dots || progress || growl
+  karma.reporters = ['progress'];
 
-// cli runner port
-runnerPort = 9100;
+  // web server port
+  karma.port = 8080;
 
-// enable / disable colors in the output (reporters and logs)
-colors = true;
+  // cli runner port
+  karma.runnerPort = 9100;
 
-// level of logging
-// possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
-logLevel = LOG_INFO;
+  // enable / disable colors in the output (reporters and logs)
+  karma.colors = true;
 
-// enable / disable watching file and executing tests whenever any file changes
-autoWatch = false;
+  // level of logging
+  // possible values: LOG_DISABLE || LOG_ERROR || LOG_WARN || LOG_INFO || LOG_DEBUG
+  karma.logLevel = karma.LOG_INFO;
 
-// Start these browsers, currently available:
-// - Chrome
-// - ChromeCanary
-// - Firefox
-// - Opera
-// - Safari (only Mac)
-// - PhantomJS
-// - IE (only Windows)
-browsers = ['Chrome'];
+  // enable / disable watching file and executing tests whenever any file changes
+  karma.autoWatch = false;
 
-// If browser does not capture in given timeout [ms], kill it
-captureTimeout = 5000;
+  // Start these browsers, currently available:
+  // - Chrome
+  // - ChromeCanary
+  // - Firefox
+  // - Opera
+  // - Safari (only Mac)
+  // - PhantomJS
+  // - IE (only Windows)
+  karma.browsers = ['Chrome'];
 
-// Continuous Integration mode
-// if true, it capture browsers, run tests and exit
-singleRun = false;
+  // If browser does not capture in given timeout [ms], kill it
+  karma.captureTimeout = 5000;
+
+  // Continuous Integration mode
+  // if true, it capture browsers, run tests and exit
+  karma.singleRun = false;
+}
